a0bec633918aca0438cc10694c96ef557f35881d
jeremeyang <jeremeyang@ufl.edu>
2026-02-25 18:08:13 -0500

    feat: add signin functionality

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -1,21 +1,36 @@
 "use server";
 
-import { auth } from "@/lib/auth"
+import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
 export async function signUpAction(formData: FormData) {
-    const email = formData.get("email") as string; // TODO: validate
-    const password = formData.get("password") as string;
-    const name = formData.get("name") as string;
+  const email = formData.get("email") as string; // TODO: validate
+  const password = formData.get("password") as string;
+  const name = formData.get("name") as string;
 
-    // send to backend
-    await auth.api.signUpEmail({
-        body: {
-            email,
-            password,
-            name,
-        }
-    })
+  // send to backend
+  await auth.api.signUpEmail({
+    body: {
+      email,
+      password,
+      name,
+    },
+  });
 
-    redirect("/")
+  redirect("/");
+}
+
+export async function signInAction(formData: FormData) {
+  const email = formData.get("email") as string; // TODO: validate
+  const password = formData.get("password") as string;
+
+  // send to backend
+  await auth.api.signInEmail({
+    body: {
+      email,
+      password,
+    },
+  });
+
+  redirect("/");
 }
