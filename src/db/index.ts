3781ce092b309f3d2a5307efea4c26f57f791671
jeremeyang <jeremeyang@ufl.edu>
2026-02-25 17:37:02 -0500

    fix: add database conn string type safety

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,10 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/mysql2";
+const connectionString = process.env.DATABASE_URL;
 
-const db = drizzle(process.env.DATABASE_URL);
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined in your environment variables");
+}
+
+// Now TS knows connectionString is definitely a string
+export const db = drizzle(connectionString);
